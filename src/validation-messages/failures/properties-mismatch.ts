6e1420d7508f9f2f689f27fca3c9f1222fbbc915
Christine Yost Kies <yost90@gmail.com>
2021-04-26 11:47:30 -0600

    Update failure message for property mismatch validation failure (#31)
    * Update failure message for property mismatch validation failure
    
    * Update PropertiesMismatch variable names

--- src/validation-messages/failures/properties-mismatch.ts
+++ src/validation-messages/failures/properties-mismatch.ts
@@ -3,15 +3,20 @@ import ValidationFailure from './validation-failure';
 class PropertiesMismatch extends ValidationFailure {
   constructor(
     path: string[],
-    schemaProperties: string[],
-    actualProperties: string[],
+    schemaPropertiesNotFound: string[],
+    unexpectedActualProperties: string[],
   ) {
-    super(
-      `Actual object contains a property not present in schema. Schema properties: ${schemaProperties.join(
-        ', ',
-      )}. Actual properties: ${actualProperties.join(', ')}.`,
-      path,
-    );
+    let message = `Actual object contains a property not present in schema. Actual properties not expected: ${unexpectedActualProperties.join(
+      ', ',
+    )}.`;
+
+    if (schemaPropertiesNotFound.length > 0) {
+      message = message.concat(
+        ` Schema properties not found: ${schemaPropertiesNotFound.join(', ')}.`,
+      );
+    }
+
+    super(message, path);
   }
 }
 
