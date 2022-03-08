78eca19565c0452c90cc077f6cbdf9be3a8e546b
Steve Long <2211897+stevelong00@users.noreply.github.com>
2022-03-08 14:44:30 -0500

    API-13897 Fix IDE errors in validator tests
    Using a constant of type ParameterWithSchema where appropriate.

--- test/utilities/example-group/example-group.factory.test.ts
+++ test/utilities/example-group/example-group.factory.test.ts
@@ -1,3 +1,4 @@
+import { ParameterWithSchema } from 'swagger-client';
 import { ExampleGroupFactory } from '../../../src/utilities/example-group';
 import OASOperation from '../../../src/utilities/oas-operation';
 
@@ -313,7 +314,7 @@ describe('ExampleGroupFactory', () => {
 
     describe('the example is set on the parameter and schema', () => {
       it('returns the example found on the parameter', () => {
-        const raceParameter = {
+        const raceParameter: ParameterWithSchema = {
           name: 'race',
           in: 'query',
           required: true,
@@ -342,7 +343,7 @@ describe('ExampleGroupFactory', () => {
 
     describe('the example is set on the schema only', () => {
       it('returns the example found on the schema', () => {
-        const raceParameter = {
+        const raceParameter: ParameterWithSchema = {
           name: 'race',
           in: 'query',
           required: true,
@@ -367,7 +368,7 @@ describe('ExampleGroupFactory', () => {
         });
       });
       it('returns the non required example found on the schema', () => {
-        const raceParameter = {
+        const raceParameter: ParameterWithSchema = {
           name: 'race',
           in: 'query',
           required: false,
@@ -395,7 +396,7 @@ describe('ExampleGroupFactory', () => {
 
     describe('the example is set on the content only', () => {
       it('returns the example found on the content', () => {
-        const raceParameter = {
+        const raceParameter: ParameterWithSchema = {
           name: 'race',
           in: 'query',
           required: true,
@@ -424,7 +425,7 @@ describe('ExampleGroupFactory', () => {
         });
       });
       it('returns the non required example found on the content', () => {
-        const raceParameter = {
+        const raceParameter: ParameterWithSchema = {
           name: 'race',
           in: 'query',
           required: false,
