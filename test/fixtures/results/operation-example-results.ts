f823ad4f41242b58c679061324c66f1c430bf85c
Steve Long <2211897+stevelong00@users.noreply.github.com>
2022-06-22 16:52:42 -0400

    Adjusted how some operation-example-results fixtures are declared

--- test/fixtures/results/operation-example-results.ts
+++ test/fixtures/results/operation-example-results.ts
@@ -30,38 +30,38 @@ export const operationExampleResultFailuresWarningsString = `  GET:/harryPotter 
     - Warning: This object is missing non-required properties that were unable to be validated, including middleName. Path: body. Found 2 times
 `;
 
-export const operationExampleResultFailuresWarningsStructure = (function () {
-  const result: ApiResults = {
-    apiSummary: {
-      totalPass: 0,
-      totalWarn: 1,
-      totalFail: 1,
-      totalRun: 1,
-      runDateTime: mockedSystemTime,
-    },
-  };
-  result[operationExampleResultFailuresWarnings.operationId] = {
-    endpointSummary: {
-      totalPass: 0,
-      totalWarn: 1,
-      totalFail: 1,
-      totalRun: 1,
-    },
-  };
-  result[operationExampleResultFailuresWarnings.operationId][
-    operationExampleResultFailuresWarnings.exampleGroupName
-  ] = {
-    errors: [...responseFailuresMap].map(([, value]) => ({
-      message: value.message,
-      count: value.count,
-    })),
-    warnings: [...responseOneWarningMap].map(([, value]) => ({
-      message: value.message,
-      count: value.count,
-    })),
-  };
-  return result;
-})();
+const operationExampleResultFailuresWarningsStructure: ApiResults = {
+  apiSummary: {
+    totalPass: 0,
+    totalWarn: 1,
+    totalFail: 1,
+    totalRun: 1,
+    runDateTime: mockedSystemTime,
+  },
+};
+operationExampleResultFailuresWarningsStructure[
+  operationExampleResultFailuresWarnings.operationId
+] = {
+  endpointSummary: {
+    totalPass: 0,
+    totalWarn: 1,
+    totalFail: 1,
+    totalRun: 1,
+  },
+};
+operationExampleResultFailuresWarningsStructure[
+  operationExampleResultFailuresWarnings.operationId
+][operationExampleResultFailuresWarnings.exampleGroupName] = {
+  errors: [...responseFailuresMap].map(([, value]) => ({
+    message: value.message,
+    count: value.count,
+  })),
+  warnings: [...responseOneWarningMap].map(([, value]) => ({
+    message: value.message,
+    count: value.count,
+  })),
+};
+export { operationExampleResultFailuresWarningsStructure };
 
 export const operationExampleResultFailuresNoWarnings =
   new OperationExampleResult(
@@ -76,35 +76,35 @@ export const operationExampleResultFailuresNoWarningsString = `  GET:/he-who-mus
     - Actual object missing required property. Required property: house. Path: body. Found 1 time
 `;
 
-export const operationExampleResultFailuresNoWarningsStructure = (function () {
-  const result: ApiResults = {
-    apiSummary: {
-      totalPass: 0,
-      totalWarn: 0,
-      totalFail: 1,
-      totalRun: 1,
-      runDateTime: mockedSystemTime,
-    },
-  };
-  result[operationExampleResultFailuresNoWarnings.operationId] = {
-    endpointSummary: {
-      totalPass: 0,
-      totalWarn: 0,
-      totalFail: 1,
-      totalRun: 1,
-    },
-  };
-  result[operationExampleResultFailuresNoWarnings.operationId][
-    operationExampleResultFailuresNoWarnings.exampleGroupName
-  ] = {
-    errors: [...responseOneFailureMap].map(([, value]) => ({
-      message: value.message,
-      count: value.count,
-    })),
-    warnings: [],
-  };
-  return result;
-})();
+const operationExampleResultFailuresNoWarningsStructure: ApiResults = {
+  apiSummary: {
+    totalPass: 0,
+    totalWarn: 0,
+    totalFail: 1,
+    totalRun: 1,
+    runDateTime: mockedSystemTime,
+  },
+};
+operationExampleResultFailuresNoWarningsStructure[
+  operationExampleResultFailuresNoWarnings.operationId
+] = {
+  endpointSummary: {
+    totalPass: 0,
+    totalWarn: 0,
+    totalFail: 1,
+    totalRun: 1,
+  },
+};
+operationExampleResultFailuresNoWarningsStructure[
+  operationExampleResultFailuresNoWarnings.operationId
+][operationExampleResultFailuresNoWarnings.exampleGroupName] = {
+  errors: [...responseOneFailureMap].map(([, value]) => ({
+    message: value.message,
+    count: value.count,
+  })),
+  warnings: [],
+};
+export { operationExampleResultFailuresNoWarningsStructure };
 
 export const operationExampleResultNoFailuresWarnings =
   new OperationExampleResult(
@@ -120,35 +120,35 @@ export const operationExampleResultNoFailuresWarningsString = `  GET:/he-who-mus
     - Warning: This object is missing non-required properties that were unable to be validated, including address2. Path: body -> horcruxes -> location. Found 1 time
 `;
 
-export const operationExampleResultNoFailuresWarningsStructure = (function () {
-  const result: ApiResults = {
-    apiSummary: {
-      totalPass: 1,
-      totalWarn: 1,
-      totalFail: 0,
-      totalRun: 1,
-      runDateTime: mockedSystemTime,
-    },
-  };
-  result[operationExampleResultNoFailuresWarnings.operationId] = {
-    endpointSummary: {
-      totalPass: 1,
-      totalWarn: 1,
-      totalFail: 0,
-      totalRun: 1,
-    },
-  };
-  result[operationExampleResultNoFailuresWarnings.operationId][
-    operationExampleResultNoFailuresWarnings.exampleGroupName
-  ] = {
-    errors: [],
-    warnings: [...responseWarningsMap].map(([, value]) => ({
-      message: value.message,
-      count: value.count,
-    })),
-  };
-  return result;
-})();
+const operationExampleResultNoFailuresWarningsStructure: ApiResults = {
+  apiSummary: {
+    totalPass: 1,
+    totalWarn: 1,
+    totalFail: 0,
+    totalRun: 1,
+    runDateTime: mockedSystemTime,
+  },
+};
+operationExampleResultNoFailuresWarningsStructure[
+  operationExampleResultNoFailuresWarnings.operationId
+] = {
+  endpointSummary: {
+    totalPass: 1,
+    totalWarn: 1,
+    totalFail: 0,
+    totalRun: 1,
+  },
+};
+operationExampleResultNoFailuresWarningsStructure[
+  operationExampleResultNoFailuresWarnings.operationId
+][operationExampleResultNoFailuresWarnings.exampleGroupName] = {
+  errors: [],
+  warnings: [...responseWarningsMap].map(([, value]) => ({
+    message: value.message,
+    count: value.count,
+  })),
+};
+export { operationExampleResultNoFailuresWarningsStructure };
 
 export const operationExampleResultRequestValidationFailures =
   new OperationExampleResult(
@@ -168,54 +168,56 @@ export const operationExampleResultNoRequestValidationFailures =
     requestResponseWarningMap,
   );
 
-export const operationExampleResultMixedStructure = (function () {
-  const result: ApiResults = {
-    apiSummary: {
-      totalPass: 1,
-      totalWarn: 2,
-      totalFail: 2,
-      totalRun: 3,
-      runDateTime: mockedSystemTime,
-    },
-  };
+const operationExampleResultMixedStructure: ApiResults = {
+  apiSummary: {
+    totalPass: 1,
+    totalWarn: 2,
+    totalFail: 2,
+    totalRun: 3,
+    runDateTime: mockedSystemTime,
+  },
+};
 
-  // FailuresWarnings - Operation ID GET:/harryPotter
-  result[operationExampleResultFailuresWarnings.operationId] =
-    operationExampleResultFailuresWarningsStructure[
-      operationExampleResultFailuresWarnings.operationId
-    ];
-  result[operationExampleResultFailuresWarnings.operationId][
-    operationExampleResultFailuresWarnings.exampleGroupName
-  ] =
-    operationExampleResultFailuresWarningsStructure[
-      operationExampleResultFailuresWarnings.operationId
-    ][operationExampleResultFailuresWarnings.exampleGroupName];
+// FailuresWarnings - Operation ID GET:/harryPotter
+operationExampleResultMixedStructure[
+  operationExampleResultFailuresWarnings.operationId
+] =
+  operationExampleResultFailuresWarningsStructure[
+    operationExampleResultFailuresWarnings.operationId
+  ];
+operationExampleResultMixedStructure[
+  operationExampleResultFailuresWarnings.operationId
+][operationExampleResultFailuresWarnings.exampleGroupName] =
+  operationExampleResultFailuresWarningsStructure[
+    operationExampleResultFailuresWarnings.operationId
+  ][operationExampleResultFailuresWarnings.exampleGroupName];
 
-  // FailuresNoWarnings & NoFailuresWarnings share the same Operation ID - GET:/he-who-must-not-be-named
-  result[operationExampleResultFailuresNoWarnings.operationId] = {
-    endpointSummary: {
-      totalPass: 1,
-      totalWarn: 1,
-      totalFail: 1,
-      totalRun: 2,
-    },
-  };
+// FailuresNoWarnings & NoFailuresWarnings share the same Operation ID - GET:/he-who-must-not-be-named
+operationExampleResultMixedStructure[
+  operationExampleResultFailuresNoWarnings.operationId
+] = {
+  endpointSummary: {
+    totalPass: 1,
+    totalWarn: 1,
+    totalFail: 1,
+    totalRun: 2,
+  },
+};
 
-  // FailuresNoWarnings
-  result[operationExampleResultFailuresNoWarnings.operationId][
-    operationExampleResultFailuresNoWarnings.exampleGroupName
-  ] =
-    operationExampleResultFailuresNoWarningsStructure[
-      operationExampleResultFailuresNoWarnings.operationId
-    ][operationExampleResultFailuresNoWarnings.exampleGroupName];
+// FailuresNoWarnings
+operationExampleResultMixedStructure[
+  operationExampleResultFailuresNoWarnings.operationId
+][operationExampleResultFailuresNoWarnings.exampleGroupName] =
+  operationExampleResultFailuresNoWarningsStructure[
+    operationExampleResultFailuresNoWarnings.operationId
+  ][operationExampleResultFailuresNoWarnings.exampleGroupName];
 
-  // NoFailuresWarnings
-  result[operationExampleResultNoFailuresWarnings.operationId][
-    operationExampleResultNoFailuresWarnings.exampleGroupName
-  ] =
-    operationExampleResultNoFailuresWarningsStructure[
-      operationExampleResultNoFailuresWarnings.operationId
-    ][operationExampleResultNoFailuresWarnings.exampleGroupName];
+// NoFailuresWarnings
+operationExampleResultMixedStructure[
+  operationExampleResultNoFailuresWarnings.operationId
+][operationExampleResultNoFailuresWarnings.exampleGroupName] =
+  operationExampleResultNoFailuresWarningsStructure[
+    operationExampleResultNoFailuresWarnings.operationId
+  ][operationExampleResultNoFailuresWarnings.exampleGroupName];
 
-  return result;
-})();
+export { operationExampleResultMixedStructure };
