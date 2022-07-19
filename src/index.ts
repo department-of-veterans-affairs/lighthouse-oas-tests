99751fde820a68a511d3a1855751e454aeb299da
mheusgit <marc.heusser@adhocteam.us>
2022-07-19 13:44:09 -0700

    feat: Add json converter access

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import JSONStructuredOutputFactory from './utilities/structured-output';
+
 export { run } from '@oclif/command';
 export { PositiveConductor } from './conductors';
 export { OASResult } from './results';
 export { TestOptions } from './config';
+export { JSONStructuredOutputFactory };
