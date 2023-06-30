d9da8fbbaf44751bb954eeff5510d1da8374f071
marc.heusser@adhocteam.us <marc.heusser@adhocteam.us>
2023-06-30 15:29:17 -0700

    fix: API-26681 DocServer & LOAST: Upgrade typescript version - Update typescript & migrated oclif to oclif/core
    Signed-off-by: marc.heusser@adhocteam.us <marc.heusser@adhocteam.us>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import JSONStructuredOutputFactory from './utilities/structured-output';
 
-export { run } from '@oclif/command';
+export { run } from '@oclif/core';
 export { default as Loast } from './loast';
 export { OASResult } from './validation';
 export { TestOptions } from './config';
