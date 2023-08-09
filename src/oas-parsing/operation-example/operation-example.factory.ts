ffd9149427398c30306cd7414a88ec21411ce3d0
Christine <yost90@gmail.com>
2023-08-09 16:37:46 -0600

    refactor(operationexamplefactory): aPI-28061 - update operation-example.factory.ts logic

--- src/oas-parsing/operation-example/operation-example.factory.ts
+++ src/oas-parsing/operation-example/operation-example.factory.ts
@@ -6,45 +6,26 @@ export default class OperationExampleFactory {
   public static buildFromOperations(
     operations: OASOperation[],
   ): OperationExample[] {
-    let operationExamples: OperationExample[] = [];
-
-    operations.forEach((operation) => {
-      operationExamples = [
-        ...operationExamples,
-        ...this.buildFromOperation(operation),
-      ];
-    });
-
-    return operationExamples;
+    return operations.flatMap((operation) =>
+      this.buildFromOperation(operation),
+    );
   }
 
   private static buildFromOperation(
     operation: OASOperation,
   ): OperationExample[] {
-    let operationExamples: OperationExample[] = [];
-
-    operation.exampleGroups.forEach((exampleGroup) => {
-      operationExamples = [
-        ...operationExamples,
-        ...this.buildFromOperationAndExampleGroup(operation, exampleGroup),
-      ];
-    });
-
-    return operationExamples;
+    return operation.exampleGroups.flatMap((exampleGroup) =>
+      this.buildFromOperationAndExampleGroup(operation, exampleGroup),
+    );
   }
 
   private static buildFromOperationAndExampleGroup(
     operation: OASOperation,
     exampleGroup: ExampleGroup,
   ): OperationExample[] {
-    const operationExamples: OperationExample[] = [];
-
-    operation.exampleRequestBodies.forEach((exampleRequestBody) => {
-      operationExamples.push(
+    return operation.exampleRequestBodies.map(
+      (exampleRequestBody) =>
         new OperationExample(operation, exampleGroup, exampleRequestBody),
-      );
-    });
-
-    return operationExamples;
+    );
   }
 }
